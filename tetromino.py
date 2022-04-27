5f3ebed36fcb447c0a8ecf02cf7c7504de117a69
Ryan Yang <y.ryan.yang@gmail.com>
2022-04-27 19:39:37 -0400

    switched to dynamic programming

--- tetromino.py
+++ tetromino.py
@@ -1,4 +1,5 @@
 import copy
+import time
 from pentomino import pentomino_pieces
 #todo: check the size of connected components, 
 #if size <d: nimval = 0
@@ -13,8 +14,8 @@ def mex(lst):
 				return i
 		return max(lst)+1
 
-debug = True
-def solve(board,gamemoves,level): #returns nim value
+debug = False
+def solve(board,a,b,gamemoves,level): #returns nim value
 	states = [] #a=len(board), b = len(board[0])
 	for piece in gamemoves:
 		for i in range(len(board)):
@@ -30,21 +31,60 @@ def solve(board,gamemoves,level): #returns nim value
 					else:
 						valid = False
 				if(valid):
-					states.append(solve(newboard,gamemoves,level+1))
+					states.append(solve(newboard,a,b,gamemoves, level+1))
 	if(level<=1 and debug):
 		print("one branch done")
-		# print("\n level: " + str(level))
-		# print(board)
-		# print("states: ")
-		# print(states)
-		# print("mex(states): " + str(mex(states)))
-		# print(".")
+
 	return mex(states)
+
+def BoardToInt(board,a,b):
+	val = 0
+	for i in range(a):
+		for j in range(b):
+			val += board[i][j] * (2**(b*i + j))
+	return val
+
+
+
+def dp(a,b,gamemoves):
+	dpvalues = [0 for i in range(2**(a*b))]
+	
+	for o in range(2**(a*b)):
+		board = [[0 for j in range(b)] for i in range(a)]
+		bitmask = o
+		for x in range(a):
+			for y in range(b):
+				board[x][y] = bitmask%2
+				bitmask = bitmask >> 1
+
+		states = [] #a=len(board), b = len(board[0])
+		for piece in gamemoves:
+			for i in range(a):
+				for j in range(b):
+					move = [(c[0]+i,c[1]+j) for c in piece]
+					newboard = copy.deepcopy(board)
+
+					valid = True
+					for cell in move:
+						if(cell[0]>=0 and cell[0]<len(board) and cell[1]>=0 and cell[1]<len(board[0]) and newboard[cell[0]][cell[1]]==1):
+							newboard[cell[0]][cell[1]]=0
+						else:
+							valid = False
+					if(valid):
+						# print(BoardToInt(newboard,a,b))
+						# print(len(dpvalues))
+						# print(dpvalues[256])
+						states.append(dpvalues[BoardToInt(newboard,a,b)])
+		dpvalues[o] = mex(states)
+
+
+
+	return dpvalues[2**(a*b)-1]
 
 def rectboard(a,b,gamemoves):
 	board = [[1 for j in range(b)] for i in range(a)]
 
-	return solve(board,gamemoves,0)
+	return solve(board,a,b,gamemoves,0)
 
 def main():
 	#tetromino:
@@ -58,7 +98,12 @@ def main():
 	# 	for j in range(1,6):
 	# 		print(rectboard(i,j, gamemoves),end=' ')
 	# 	print()
-	print(rectboard(x[0],x[1],pentomino_gamemoves))
+	init = time.time()
+	print("dp solution: " + str(dp(x[0],x[1], tetromino_gamemoves)))
+	print("dp time: "+ str(time.time() - init))
+	midpoint = time.time()
+	print("recursion solution: " + str(rectboard(x[0],x[1],tetromino_gamemoves)))
+	print("recursion time: " + str(time.time()-midpoint))
 
 if __name__ == "__main__":
 	main()
