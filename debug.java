ea6292a3465cabe88fc115b4c7e00c539fc01974
Ryan Yang <y.ryan.yang@gmail.com>
2022-05-18 16:24:17 -0400

    working DFS implementation

--- debug.java
+++ debug.java
@@ -1,27 +1,47 @@
 public class debug {
-	public static void main(String[] args) {
-		dp test = new dp();
-		// int[][][] states = {{{0,1,1,1,3,4,5}, {0}}, {{0}}};
-		// System.out.println(test.mex(states[0][0])); //mex works.
-
-		int d = 5;
-		int[][][] gamemoves = test.returnPieces(d);
-		System.out.println(gamemoves.length);
-		for (int x=0;x<gamemoves.length; x++ ) {
-			if(x==31){
-				System.out.print("[");
-				for (int i=0;i<d ;i++ ) {
-					System.out.print("(");
-					for (int j=0;j<2 ;j++ ) {
-						System.out.print(gamemoves[x][i][j]+", ");
-					}
-					System.out.print("),");
-				}
-				System.out.print("], ");
-				System.out.println();					
+	public static int replace(int[][] inputBoard) {
+		int[][] board = new int[5][5];
+		for (int i=0; i<5;i++ ) {
+			for (int j=0;j<5 ;j++ ) {
+				board[i][j] = inputBoard[i][j];
 			}
-		
 		}
+		board[1][1] = 2;
+		return 50;
+	}
+	public static void main(String[] args) {
+		// dp test = new dp();
+		// // int[][][] states = {{{0,1,1,1,3,4,5}, {0}}, {{0}}};
+		// // System.out.println(test.mex(states[0][0])); //mex works.
 
+		// int d = 5;
+		// int[][][] gamemoves = test.returnPieces(d);
+		// System.out.println(gamemoves.length);
+		// for (int x=0;x<gamemoves.length; x++ ) {
+		// 	if(x==31){
+		// 		System.out.print("[");
+		// 		for (int i=0;i<d ;i++ ) {
+		// 			System.out.print("(");
+		// 			for (int j=0;j<2 ;j++ ) {
+		// 				System.out.print(gamemoves[x][i][j]+", ");
+		// 			}
+		// 			System.out.print("),");
+		// 		}
+		// 		System.out.print("], ");
+		// 		System.out.println();					
+		// 	}
+		
+		// }
+
+		int[][] board = new int[5][5];
+
+		replace(board);
+
+		for (int i=0;i<board.length ;i++ ) {
+			for (int j=0;j<board[0].length ;j++ ) {
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
---
+++ dpWithDFS.java
@@ -0,0 +1,165 @@
+import java.util.*;
+
+public class dpWithDFS {
+	public static int mexOcc(int[] lst, int n) {
+		for (int i=0;i<n ;i++ ) {
+			if(lst[i]==0){
+				return i;
+			}
+		}
+		return n;
+	}
+
+	public static int BoardToInt(int[][] board,int a,int b) {
+		int val = 0;
+		for (int i=0;i<a ;i++ ) {
+			for (int j=0;j<b ;j++ ) {
+				val += board[i][j] * (Math.pow(2,b*i+j));
+			}
+		}
+		return val;
+	}
+
+	public static int hammingWeight(int o){
+		int sum = 0;
+		while(o>0) {
+			sum += o%2;
+			o = o/2;
+		}
+		return sum;
+	}
+
+	public static int[] dfs(int n, int[][] movesearchInput, int d, int i, int j, int[] dpvalues, int[][] boardInput, boolean debug) {
+		int[][] movesearch = new int[movesearchInput.length][movesearchInput[0].length];
+		int[][] board = new int[boardInput.length][boardInput[0].length];
+		for (int x=0;x<board.length ;x++ ) {
+			for (int y=0;y<board[0].length ;y++) {
+				movesearch[x][y] = movesearchInput[x][y];
+				board[x][y] = boardInput[x][y];
+			}
+		}
+		movesearch[i][j] = 0;
+		board[i][j] = 0;
+		int[] nimocc = new int[n];
+
+
+		// System.out.println("Movesearch: ");
+		// for (int x=0;x<board.length ;x++ ) {
+		// 	for (int y=0;y<board[0].length ;y++) {
+		// 		System.out.print(movesearch[x][y]+" ");
+		// 	}
+		// 	System.out.println();
+		// }
+		// System.out.println("--\n");	
+		if(d==1){
+			nimocc[dpvalues[BoardToInt(board,board.length, board[0].length)]]++;
+
+			if(debug) {
+				System.out.println("at level:" + d);
+				for (int x=0;x<board.length ;x++ ) {
+					for (int y=0;y<board[0].length ;y++) {
+						System.out.print(board[x][y]+" ");
+					}
+					System.out.println();
+				}			
+				System.out.println();				
+			}
+		}
+		else {
+			int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
+			for (int c=0;c<4 ;c++ ) {
+				int x = directions[c][0];
+				int y = directions[c][1];
+
+				if(0<=i+x && i+x<board.length && 0<=j+y && j+y<board[0].length && movesearch[i+x][j+y] == 1){
+					movesearch[i+x][j+y] = 0;
+					int[] branchValues = dfs(n, movesearch, d-1, i+x, j+y, dpvalues, board, debug);
+					movesearch[i+x][j+y] = 1;
+					for (int k=0;k<n ;k++ ) {
+						nimocc[k] += branchValues[k];
+					}
+				}				
+			}
+	
+		}
+		return nimocc;
+	}
+
+	public static int dp(int a, int b, int d){
+		boolean debug = false;
+		final long startTime = System.currentTimeMillis();		
+		int[] dpvalues = new int[(int) Math.pow(2,a*b)];
+
+		for (int o=0;o<Math.pow(2,a*b) ;o++ ) {
+			int[][] board = new int[a][b];
+			if(!debug){
+				int p = 10;
+				if((o % (Math.pow(2,a*b-p))) == 0){
+					System.out.println(o + " out of " + Math.pow(2,a*b) + " (out of " + Math.pow(2,p) + " this is " + o/Math.pow(2,a*b-p) + ")");
+					System.out.println("Total execution time so far (seconds): " + ((System.currentTimeMillis() - startTime))/1000.0);						
+				}
+			}
+
+			if (((a*b-hammingWeight(o))%d)==0) {
+				// System.out.println(o + " Hamming Weight: " + hammingWeight(o));
+				int bitmask = o;
+				for (int i=0;i<a ;i++ ) {
+					for (int j=0;j<b ;j++ ) {
+						board[i][j] = bitmask %2;
+						// System.out.println("board[i][j]: " + board[i][j]);
+						bitmask = bitmask/2;
+					}
+				}
+
+				int[][] movesearch = new int[a][b];
+				for (int x=0;x<a ;x++ ) {
+					for (int y=0;y<b ;y++ ) {
+						movesearch[x][y] = board[x][y];
+					}
+				}	
+				
+				int n = (a*b)/d;
+				int[] nimocc = new int[n];
+				int numLegalMoves = 0;
+				for (int i=0;i<a ;i++) {
+					for (int j=0;j<b ;j++ ) {
+						if(movesearch[i][j]==1){
+							int[] positionValues = dfs(n, movesearch, d, i, j, dpvalues, board,debug);							
+							// movesearch[i][j] = 0;
+
+							for (int k=0;k<n ;k++ ) {
+								if(debug){
+									System.out.print(positionValues[k] + " ");									
+								}
+								nimocc[k] += positionValues[k];
+								numLegalMoves += positionValues[k];
+							}
+							if(debug){
+								System.out.println();								
+							}
+						}
+					}
+				}
+				if(debug) {
+					for (int i=0;i<n ;i++ ) {
+						System.out.print(nimocc[i] + " ");
+					}
+					System.out.println("numLegalMoves: " + numLegalMoves);
+					System.out.println();					
+				}
+				dpvalues[o] = mexOcc(nimocc,n);
+			}
+			// System.out.println(o + " " + dpvalues[o]);
+		}
+		return dpvalues[(int) Math.pow(2,a*b)-1];
+	}
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		int a = console.nextInt();
+		int b = console.nextInt();
+		int d = console.nextInt();
+		System.out.println(dp(a,b,d));
+
+	}
+}
