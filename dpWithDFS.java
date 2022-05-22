50a170e59105ebcb5bd2c46992bfe369e0c9b9ad
Ryan Yang <y.ryan.yang@gmail.com>
2022-05-21 21:28:18 -0400

    added possible tests. also cleaned up system.out

--- dpWithDFS.java
+++ dpWithDFS.java
@@ -87,13 +87,14 @@ public class dpWithDFS {
 
 	public static int dp(int a, int b, int d){
 		boolean debug = false;
+		boolean percents = false;
 		final long startTime = System.currentTimeMillis();		
 		int[] dpvalues = new int[(int) Math.pow(2,a*b)];
 
 		for (int o=0;o<Math.pow(2,a*b) ;o++ ) {
 			int[][] board = new int[a][b];
-			if(!debug){
-				int p = 9;
+			if(percents){
+				int p = 7;
 				if((o % (Math.pow(2,a*b-p))) == 0){
 					System.out.println(o + " out of " + Math.pow(2,a*b) + " (out of " + Math.pow(2,p) + " this is " + o/Math.pow(2,a*b-p) + ")");
 					System.out.println("Total execution time so far (seconds): " + ((System.currentTimeMillis() - startTime))/1000.0);						
@@ -140,7 +141,7 @@ public class dpWithDFS {
 						}
 					}
 				}
-				if(debug || (o==(int)Math.pow(2,a*b)-1)) {
+				if(debug || (percents && o==(int)Math.pow(2,a*b)-1)) {
 					for (int i=0;i<n ;i++ ) {
 						System.out.print(nimocc[i] + " ");
 					}
@@ -156,10 +157,59 @@ public class dpWithDFS {
 
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
-		int a = console.nextInt();
-		int b = console.nextInt();
-		int d = console.nextInt();
-		System.out.println(dp(a,b,d));
+		final long globalStartTime = System.currentTimeMillis();	
+		// Test 1:	
+		// int a = console.nextInt();
+		// int b = console.nextInt();
+		// int d = console.nextInt();
+		// System.out.println(dp(a,b,d));
+
+		// Test 2
+		// int d = console.nextInt();
+		int tablesize = 8;
+		int maxboardsize = 20;
+		// int[][] nimvalues = new int[tablesize+1][tablesize+1];
+
+		for(int a=1; a<=tablesize; a++ ) {
+			for(int b=1; b<=tablesize; b++) {
+				if(a*b<=maxboardsize) {//next available is 27
+					// nimvalues[a][b] = dp(a,b,d);
+					System.out.println(a+"x"+b);
+					boolean allP1 = true;
+					for(int d=1; d<=10; d++){
+						// System.out.println(d+": " + dp(a,b,d));
+						if(dp(a,b,d)==0){
+							allP1 = false;
+						}
+					}
+					if(allP1){
+						System.out.println(a+"x"+b +" board is all P1 win");						
+					}
+				}
+			}
+		}
+		// for(int a=1; a<=tablesize; a++ ) {
+		// 	System.out.print(a+" & ");
+		// 	for(int b=1; b<=tablesize; b++) {
+		// 		if(a*b<=maxboardsize) {
+		// 			if(a<=b){
+		// 				System.out.print(nimvalues[a][b]);
+		// 			}
+		// 			else {
+		// 				System.out.print("\\s{" +nimvalues[a][b]+"}");
+		// 			}
+		// 		}
+		// 		if(b<tablesize){
+		// 			System.out.print(" & ");
+		// 		}				
+		// 	}
+		// 	System.out.println("\\\\");
+		// }
+		System.out.println("Total execution time (seconds): " + ((System.currentTimeMillis() - globalStartTime))/1000.0);
+
+		// Test 3
+
 
 	}
+
 }
